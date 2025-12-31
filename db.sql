57e592d1276065d1898d888ef730781f6457ca59
felimomo <felimomouni@gmail.com>
2025-12-31 08:44:33 -0600

    initial commit

---
+++ db.sql
@@ -0,0 +1,73 @@
+-- postgre SQL database
+
+CREATE DATABASE sql4fisheries;
+
+USE sql4fisheries;
+
+CREATE TABLE environments (
+	env_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	n_observ INTEGER,
+	observ_noise FLOAT CHECK (observ_noise BETWEEN 0 AND 1),
+	utility_fn TEXT CHECK (utility_fn IN ('harvest', 'hara', 'trophy')),
+	url TEXT
+);
+
+CREATE TABLE policies (
+	pol_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	training_env INTEGER, -- environments.env_id
+	algo TEXT,
+	fn_structure TEXT,
+	url TEXT,
+	CONSTRAINT fk_env
+		FOREIGN KEY (training_env)
+		REFERENCES environments(env_id)
+);
+
+CREATE TABLE rewards (
+	pol_id INTEGER,
+	env_id INTEGER,
+	episode INTEGER,
+	reward FLOAT,
+	CONSTRAINT pk_rew
+		PRIMARY KEY (pol_id, env_id, episode),
+	CONSTRAINT fk_pol
+		FOREIGN KEY (pol_id)
+		REFERENCES policies(pol_id),
+	CONSTRAINT fk_env
+		FOREIGN KEY (env_id)
+		REFERENCES environments(env_id),
+);
+
+CREATE TABLE episode_state_sims(
+	pol_id INTEGER,
+	env_id INTEGER,
+	episode INTEGER,
+	t INTEGER,
+	variable TEXT,
+	value FLOAT,
+	CONSTRAINT pk_ep_state
+		PRIMARY KEY (pol_id, env_id, episode, t),
+	CONSTRAINT fk_pol
+		FOREIGN KEY (pol_id)
+		REFERENCES policies(pol_id),
+	CONSTRAINT fk_env
+		FOREIGN KEY (env_id)
+		REFERENCES environments(env_id),
+);
+
+CREATE TABLE episode_action_sims(
+	pol_id INTEGER,
+	env_id INTEGER,
+	episode INTEGER,
+	t INTEGER,
+	variable TEXT,
+	value FLOAT,
+	CONSTRAINT pk_ep_state
+		PRIMARY KEY (pol_id, env_id, episode, t),
+	CONSTRAINT fk_pol
+		FOREIGN KEY (pol_id)
+		REFERENCES policies(pol_id),
+	CONSTRAINT fk_env
+		FOREIGN KEY (env_id)
+		REFERENCES environments(env_id),
+);
---
+++ envs.sql
@@ -0,0 +1,11 @@
+-- no need to manually add in env_id bc of auto-increase
+INSERT INTO environments (
+	n_observ, observ_noise, utility_fn, url
+)
+VALUES 
+	(1, 0.1, 'harvest', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
+	(2, 0.1, 'harvest', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
+	(1, 0.1, 'hara', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
+	(2, 0.1, 'hara', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
+	(1, 0.1, 'trophy', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
+	(2, 0.1, 'trophy', 'https://github.com/boettiger-lab/rl4fisheries/blob/main/src/rl4fisheries/envs/asm_env.py'),
