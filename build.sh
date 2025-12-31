71dd322f9eba854b312ae1088b7090ebea88e52a
felimomo <felimomouni@gmail.com>
2025-12-31 13:45:32 -0600

    bash quick build and drop

---
+++ build.sh
@@ -0,0 +1,5 @@
+psql -U postgres -d postgres -f db.sql
+psql -U postgres -d postgres -f envs.sql
+psql -U postgres -d postgres -f pols.sql
+psql -U postgres -d postgres -f rews.sql
+
---
+++ drop.sh
@@ -0,0 +1,3 @@
+# just a shortcut since I'll be using it a lot
+
+psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS sql4fisheries;"
