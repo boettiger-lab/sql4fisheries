840149df8482e65e4bfcd90833f69e33ecb2013a
felimomo <felimomouni@gmail.com>
2025-12-31 13:45:17 -0600

    gitignore, db.sql bug

--- db.sql
+++ db.sql
@@ -2,7 +2,9 @@
 
 CREATE DATABASE sql4fisheries;
 
-USE sql4fisheries;
+-- psql-specific, Equivalent to USE statement in MySQL:
+
+\c sql4fisheries 
 
 CREATE TABLE environments (
 	env_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -27,15 +29,16 @@ CREATE TABLE rewards (
 	pol_id INTEGER,
 	env_id INTEGER,
 	episode INTEGER,
+	pol_name TEXT,
 	reward FLOAT,
 	CONSTRAINT pk_rew
 		PRIMARY KEY (pol_id, env_id, episode),
 	CONSTRAINT fk_pol
 		FOREIGN KEY (pol_id)
 		REFERENCES policies(pol_id),
-	CONSTRAINT fk_env
+	CONSTRAINT fk_env_rew
 		FOREIGN KEY (env_id)
-		REFERENCES environments(env_id),
+		REFERENCES environments(env_id)
 );
 
 CREATE TABLE episode_state_sims(
@@ -45,14 +48,14 @@ CREATE TABLE episode_state_sims(
 	t INTEGER,
 	variable TEXT,
 	value FLOAT,
-	CONSTRAINT pk_ep_state
+	CONSTRAINT pk_ep_st
 		PRIMARY KEY (pol_id, env_id, episode, t),
-	CONSTRAINT fk_pol
+	CONSTRAINT fk_pol_ep_st
 		FOREIGN KEY (pol_id)
 		REFERENCES policies(pol_id),
-	CONSTRAINT fk_env
+	CONSTRAINT fk_env_ep_st
 		FOREIGN KEY (env_id)
-		REFERENCES environments(env_id),
+		REFERENCES environments(env_id)
 );
 
 CREATE TABLE episode_action_sims(
@@ -62,12 +65,12 @@ CREATE TABLE episode_action_sims(
 	t INTEGER,
 	variable TEXT,
 	value FLOAT,
-	CONSTRAINT pk_ep_state
+	CONSTRAINT pk_ep_ac
 		PRIMARY KEY (pol_id, env_id, episode, t),
-	CONSTRAINT fk_pol
+	CONSTRAINT fk_pol_ep_ac
 		FOREIGN KEY (pol_id)
 		REFERENCES policies(pol_id),
-	CONSTRAINT fk_env
+	CONSTRAINT fk_env_ep_ac
 		FOREIGN KEY (env_id)
-		REFERENCES environments(env_id),
+		REFERENCES environments(env_id)
 );
